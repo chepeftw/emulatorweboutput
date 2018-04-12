a01220557e7a8bba6fa38e808362351fa80b6366
Chepe FTW <chepeftw@gmail.com>
2018-04-12 16:03:25 +0200

    Enhancing tests for the API.

--- docker/api/api.go
+++ docker/api/api.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 
 	"gopkg.in/mgo.v2"
-	//"gopkg.in/mgo.v2/bson"
+	"gopkg.in/mgo.v2/bson"
 	"github.com/rs/cors"
 	"strings"
 	"time"
@@ -15,14 +15,19 @@ import (
 
 type EmulationResult struct {
 	Url string
+	Name string
 	Created int64
 
 	Result1 string
 }
 
-// Display a single data
-func GetICO(w http.ResponseWriter, r *http.Request) {
+type RandomResponse struct {
+	Status string
+}
+
+func SetTestData(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
+	name := strings.ToLower(params["name"])
 
 	session, err := mgo.Dial("mongodb")
 	if err != nil {
@@ -30,19 +35,47 @@ func GetICO(w http.ResponseWriter, r *http.Request) {
 	}
 	defer session.Close()
 
-	name := params["name"]
-	name = strings.ToLower(name)
+	c := session.DB("emulation").C(name)
 
-	//c := session.DB("emulation").C("version1")
+	c.Insert(&EmulationResult{
+		"http://emulator.chepeftw.com/",
+		"Random",
+		time.Now().Unix(),
+		"yes!" })
+
+	result := RandomResponse{ "OK" }
+	json.NewEncoder(w).Encode(result)
+}
+
+func GetTestData(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	name := strings.ToLower(params["name"])
+
+	session, err := mgo.Dial("mongodb")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	c := session.DB("emulation").C(name)
+
+	results := EmulationResult{}
+	err = c.Find(bson.M{"url": "http://emulator.chepeftw.com/"}).Sort("-created").Limit(1).All(&results)
+
+	json.NewEncoder(w).Encode(results)
+}
+
+// Display a single data
+func GetICO(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	name := strings.ToLower(params["name"])
 
 	results := EmulationResult{
 		Url: "https://semantic-ui.com/examples/grid.html",
+		Name: name,
 		Created: time.Now().Unix(),
 		Result1: "123456789",
-	}
-	//err = c.Find(bson.M{"name": name}).Sort("-created").Limit(1).All(&results)
-	if err != nil {
-		log.Fatal(err)
 	}
 
 	json.NewEncoder(w).Encode(results)
@@ -53,6 +86,8 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/emulation/{name}", GetICO).Methods("GET")
+	router.HandleFunc("/test/set/{name}", SetTestData).Methods("GET")
+	router.HandleFunc("/test/get/{name}", GetTestData).Methods("GET")
 
 	handler := cors.Default().Handler(router)
 	log.Fatal(http.ListenAndServe(":8000", handler))
