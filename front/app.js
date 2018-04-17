c547ddce08755925b6dfb60b2be77c6534c98af4
Chepe FTW <chepeftw@gmail.com>
2018-04-17 15:07:10 +0200

    Visual enhancements to see all possible properties.

--- front/app.js
+++ front/app.js
@@ -1,13 +1,23 @@
 console.debug("Hi");
 var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope, $http) {
-    $http.get("http://api.chepeftw.com/property/sent_messages/messages_count")
-        .then(function (response) {
 
-            $scope.sortType = 'name'; // set the default sort type
-            $scope.sortReverse = false;  // set the default sort order
-            $scope.searchFish = '';     // set the default search/filter term
+    $scope.loadData = function () {
+        var c = $('#selectCollection').val();
+        var p = $('#selectProperty').val();
 
-            $scope.sushi = response.data;
-        });
+        $('#refreshButton').html('Loading...');
+
+        // SelectElement("selectCollection", c);
+        // SelectElement("selectProperty", p);
+
+        $http.get("http://api.chepeftw.com/property/" + c + "/" + p)
+            .then(function (response) {
+                $scope.sushi = response.data;
+                $('#refreshButton').html('Refresh');
+            });
+    };
+
+    //initial load
+    $scope.loadData();
 });
