011ed17757d90e3f53b7a7b26e9098a914631d63
Chepe FTW <chepeftw@gmail.com>
2018-05-15 23:43:36 +0200

    More charts.

--- front/graph.js
+++ front/graph.js
@@ -3,50 +3,44 @@ var app = angular.module('myApp', []);
 app.controller('myCtrl', function ($scope, $http) {
 
     $scope.loadData = function () {
-        var apiURL = "http://api.chepeftw.com/graph";
+        var apiURL = "http://api.chepeftw.com/graph/monitor_query_complete/query_complete_ms/200/2";
 
-        var chartdata = {
-            chart: {
-                type: 'line'
-            },
-            title: {
-                text: 'Number of nodes'
-            },
-            xAxis: {
-                categories: ['20', '30', '40', '50']
-            },
-            yAxis: {
-                title: {
-                    text: 'Time (ms)'
-                }
-            },
-            plotOptions: {
-                line: {
-                    dataLabels: {
-                        enabled: true
-                    },
-                    enableMouseTracking: false
-                }
-            },
+        var chartdata1 = {
+            chart: { type: 'line' },
+            title: { text: 'Number of nodes' },
+            xAxis: { categories: ['20', '30', '40', '50'] },
+            yAxis: { title: { text: 'Time (ms)' } },
+            plotOptions: { line: { dataLabels: { enabled: true },  enableMouseTracking: false } },
             series: []
         };
 
-        $http.get( apiURL )
-            .then(function (response) {
-                // $scope.sushi = response.data;
+        var timeouts = [200, 300];
+        var speeds= [2, 5];
 
-                var seriesGD = [],
-                    graphData= response.data.Highchart;
+        var i, j;
+        for (i = 0; i < timeouts.length; i++) {
+            for (j = 0; i < speeds.length; j++) {
+                var apiURL1 = "http://api.chepeftw.com/graph/monitor_query_complete/query_complete_ms/" + timeouts[i] + "/" + speeds[j];
+                $http.get( apiURL1 )
+                    .then(function (response) {
+                        chartdata1.series = response.data.Highchart;
+                        Highcharts.chart('container_mqc_' + timeouts[i] + '_' + speeds[j], chartdata1);
+                    });
+            }
+        }
 
-                for (var i=0; i< graphData.length; i++) {
-                    seriesGD.push({"name" : graphData[i].name, "data" : graphData[i].data})
-                }
+        var i, j;
+        for (i = 0; i < timeouts.length; i++) {
+            for (j = 0; i < speeds.length; j++) {
+                var apiURL1 = "http://api.chepeftw.com/graph/monitor_accuracy/block_valid_ratio_percentage/" + timeouts[i] + "/" + speeds[j];
+                $http.get( apiURL1 )
+                    .then(function (response) {
+                        chartdata1.series = response.data.Highchart;
+                        Highcharts.chart('container_bvrp_' + timeouts[i] + '_' + speeds[j], chartdata1);
+                    });
+            }
+        }
 
-                chartdata.series = seriesGD;
-
-                Highcharts.chart('container', chartdata);
-
-            });
     };
 
     //initial load
