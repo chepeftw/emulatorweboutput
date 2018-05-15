e979ddb51828adba67f5224533808bf722fec5b2
Chepe FTW <chepeftw@gmail.com>
2018-05-15 16:35:59 +0200

    Adding graph method.

--- docker/api/api.go
+++ docker/api/api.go
@@ -11,6 +11,7 @@ import (
 	"github.com/rs/cors"
 	"strings"
 	"fmt"
+	"math"
 )
 
 type m bson.M // just for brevity, bson.M type is map[string]interface{}
@@ -27,8 +28,18 @@ type AggregationResult struct {
 	Nodes    float64 `bson:"nodes"`
 	Size     float64 `bson:"size"`
 	Duration float64 `bson:"duration"`
+	Speed    float64 `bson:"speed"`
 
 	Runs float64 `bson:"runs"`
+}
+
+type Highchart struct {
+	Name string    `json:"name"`
+	Data []float64 `json:"data"`
+}
+
+type Highcharts struct {
+	Highchart []Highchart
 }
 
 func GetProcessedProperty(w http.ResponseWriter, r *http.Request) {
@@ -70,10 +81,10 @@ func GetProcessedProperty(w http.ResponseWriter, r *http.Request) {
 		match = m{"$match": m{prop: m{"$gt": 10}, "name": m{"$regex": bson.RegEx{Pattern: pattern, Options: "si"}}}}
 	} else if "query_complete_ms" == prop {
 		fmt.Println("query_complete_ms filter!")
-		match = m{"$match": m{"name": m{"$regex": bson.RegEx{Pattern: pattern, Options: "si"}}, prop: m{"$lt": 50000, "$gt": 0} }}
+		match = m{"$match": m{"name": m{"$regex": bson.RegEx{Pattern: pattern, Options: "si"}}, prop: m{"$lt": 50000, "$gt": 0}}}
 	} else if "completion_time_ms" == prop {
 		fmt.Println("completion_time_ms filter!")
-		match = m{"$match": m{"name": m{"$regex": bson.RegEx{Pattern: pattern, Options: "si"}}, prop: m{"$lt": 50000, "$gt": 0} }}
+		match = m{"$match": m{"name": m{"$regex": bson.RegEx{Pattern: pattern, Options: "si"}}, prop: m{"$lt": 50000, "$gt": 0}}}
 	}
 
 	pipeLine := []m{
@@ -112,11 +123,119 @@ func GetProcessedProperty(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(result)
 }
 
+func GetProcessedGraph(w http.ResponseWriter, r *http.Request) {
+	//params := mux.Vars(r)
+	//name := strings.ToLower(params["name"])
+	//prop := strings.ToLower(params["prop"])
+	name := "monitor_query_complete"
+	prop := "query_complete_ms"
+
+	pattern := "^Blockchain"
+	db := r.URL.Query().Get("db")
+
+	fmt.Println("Receiving request for graph for name = " + name + " and property = " + prop)
+
+	dbName := "blockchain0"
+	if db != "" {
+		dbName = db
+	}
+
+	session, err := mgo.Dial("mongodb")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	c := session.DB(dbName).C(name)
+
+	property := "$" + prop
+
+	match := m{"$match": m{"name": m{"$regex": bson.RegEx{Pattern: pattern, Options: "si"}}, prop: m{"$lt": 50000, "$gt": 0}}}
+	group := m{"$group":
+	m{"_id": "$name",
+		"minVal": m{"$min": property},
+		"maxVal": m{"$max": property},
+		"avgVal": m{"$avg": property},
+		"stdPVal": m{"$stdDevPop": property},
+		"stdSVal": m{"$stdDevSamp": property},
+		"timeout": m{"$avg": "$timeout"},
+		"nodes": m{"$avg": "$nodes"},
+		"size": m{"$avg": "$size"},
+		"speed": m{"$avg": "$speed"},
+		"duration": m{"$avg": "$duration"},
+		"runs": m{"$sum": 1}},
+	}
+	sort := m{"$sort": m{"_id": 1}}
+
+	numberOfNodes := [4]int{20, 30, 40, 50}
+	timeout := 200
+	speed := 2
+
+	finalResult := Highcharts{}
+	finalResult.Highchart = append(finalResult.Highchart, Highchart{"Low density", []float64{}})
+	finalResult.Highchart = append(finalResult.Highchart, Highchart{"Medium density", []float64{}})
+	finalResult.Highchart = append(finalResult.Highchart, Highchart{"High density", []float64{}})
+
+	for _, nodes := range numberOfNodes {
+		match = m{"$match": m{"name": m{"$regex": bson.RegEx{Pattern: pattern, Options: "si"}}, prop: m{"$lt": 50000, "$gt": 0}, "timeout": timeout, "speed": speed, "nodes": nodes}}
+		pipeLine := []m{match, group, sort}
+
+		var result []AggregationResult
+		c.Pipe(pipeLine).All(&result)
+
+		sum := float64(0)
+		min := float64(1000)
+		max := float64(0)
+		for _, res := range result {
+			min = math.Min(min, res.Size)
+			max = math.Max(max, res.Size)
+			sum += res.Size
+		}
+		med := sum - min - max
+
+		minVal := float64(0)
+		medVal := float64(0)
+		maxVal := float64(0)
+		for _, res := range result {
+			switch res.Size {
+			case min:
+				minVal = res.AverageValue
+				break
+			case med:
+				medVal = res.AverageValue
+				break
+			case max:
+				maxVal = res.AverageValue
+				break
+			}
+		}
+
+		for _, chart := range finalResult.Highchart {
+			switch chart.Name {
+			case "Low density":
+				chart.Data = append(chart.Data, minVal)
+				break
+			case "Medium density":
+				chart.Data = append(chart.Data, medVal)
+				break
+			case "High density":
+				chart.Data = append(chart.Data, maxVal)
+				break
+			}
+		}
+
+	}
+
+	json.NewEncoder(w).Encode(finalResult)
+}
+
 // main function to boot up everything
 func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/property/{name}/{prop}", GetProcessedProperty).Methods("GET")
+	//router.HandleFunc("/graph/{name}/{prop}", GetProcessedGraph).Methods("GET")
+	router.HandleFunc("/graph", GetProcessedGraph).Methods("GET")
 
 	handler := cors.Default().Handler(router)
 	log.Fatal(http.ListenAndServe(":8000", handler))
